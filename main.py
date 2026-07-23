abb849204c8ea2a292659f78300cbea8237b3869
ViktorKuralesov <orjeunesse12345@gmail.com>
2026-07-23 21:58:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def handle_message(message):
             model='gemini-2.0-flash',
             contents=message.text,
         )
-        bot.reply_to(message, response.text)  # <--- Перенесите сюда (с отступом)
+        bot.reply_to(message, response.text) 
     except Exception as e:
         bot.reply_to(message, f"Пизда рулю пацаны. Кина не будет: {e}")
 
